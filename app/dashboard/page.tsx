e8f000f0d5b45f5e4b86cce815a995838b2ce7e6
Seotjuu <79149336+tjrenffl8@users.noreply.github.com>
2025-06-16 22:00:28 +0900

    [bug] build bug fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,5 @@
-"use client";
-
-import DashboardCommit from "@/component/dashboard/DashboardCommit";
-import DashboardContainer from "@/component/dashboard/DashboardContainer";
 import DashboardWrapContent from "@/component/dashboard/DashboardWrapContent";
-import {
-  dashboardContentData,
-  IReposData,
-} from "@/constants/dashboardContentData";
-import { useSession } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 //: 최근 커밋 내역: 메시지 + 이름
 // : 브랜치 현황
 // : 이슈 목록
@@ -18,51 +8,9 @@ import { Suspense, useEffect, useState } from "react";
 // : 잔디밭 1년
 
 const DashboardPage = () => {
-  const { data } = useSession();
-  const param = useSearchParams();
-  const [repo, setRepo] = useState<IReposData | null>(null);
-  const [contentData, setContentData] = useState<any[]>(dashboardContentData);
-
-  useEffect(() => {
-    // 선택한 Repository가 있을 경우
-    const repoData = localStorage.getItem("repo");
-    if (repoData) {
-      setRepo(JSON.parse(repoData));
-    } else {
-      setRepo(null);
-    }
-
-    setContentData(
-      dashboardContentData.map((content) => {
-        return {
-          ...content,
-          content:
-            content.component === "RecentCommits" ? (
-              <DashboardCommit repo={repoData} />
-            ) : (
-              ""
-            ),
-        };
-      })
-    );
-  }, [param]);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <DashboardContainer repo={repo} data={data}>
-        {contentData.map((content, index_st) => (
-          <DashboardWrapContent key={index_st}>
-            <h1 className="flex items-center gap-5">
-              {content.icon} {content.title}
-            </h1>
-            <div className="h-[300px] overflow-y-auto">
-              {content.component === "RecentCommits" ? (
-                <DashboardCommit repo={repo} />
-              ) : null}
-            </div>
-          </DashboardWrapContent>
-        ))}
-      </DashboardContainer>
+    <Suspense>
+      <DashboardWrapContent />
     </Suspense>
   );
 };
--- component/dashboard/DashboardWrapContent.tsx
+++ component/dashboard/DashboardWrapContent.tsx
@@ -1,15 +1,64 @@
+"use client";
 
-interface IProps {
-  children: React.ReactNode;
-  className?: string;
-}
+import { useSession } from "next-auth/react";
+import DashboardContainer from "./DashboardContainer";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  IReposData,
+  dashboardContentData,
+} from "@/constants/dashboardContentData";
+import DashboardCommit from "./DashboardCommit";
 
-const DashboardWrapContent = ({ children, className }: IProps) => {
+const DashboardWrapContent = () => {
+  const { data } = useSession();
+  const param = useSearchParams();
+  const [repo, setRepo] = useState<IReposData | null>(null);
+  const [contentData, setContentData] = useState<any[]>(dashboardContentData);
+
+  useEffect(() => {
+    // 선택한 Repository가 있을 경우
+    const repoData = localStorage.getItem("repo");
+    if (repoData) {
+      setRepo(JSON.parse(repoData));
+    } else {
+      setRepo(null);
+    }
+
+    setContentData(
+      dashboardContentData.map((content) => {
+        return {
+          ...content,
+          content:
+            content.component === "RecentCommits" ? (
+              <DashboardCommit repo={repoData} />
+            ) : (
+              ""
+            ),
+        };
+      })
+    );
+  }, [param]);
+
   return (
-    <div className={`w-full border border-gray-300 rounded-lg p-5 ${className}`}>
-      {children}
-    </div>
+    <DashboardContainer repo={repo} data={data}>
+      {contentData.map((content, index_st) => (
+        <div
+          key={index_st}
+          className={`w-full border border-gray-300 rounded-lg p-5 `}
+        >
+          <h1 className="flex items-center gap-5">
+            {content.icon} {content.title}
+          </h1>
+          <div className="h-[300px] overflow-y-auto">
+            {content.component === "RecentCommits" ? (
+              <DashboardCommit repo={repo} />
+            ) : null}
+          </div>
+        </div>
+      ))}
+    </DashboardContainer>
   );
-}
+};
 
 export default DashboardWrapContent;
